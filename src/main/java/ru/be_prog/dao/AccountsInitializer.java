e24c001afd8cc0ac4275b98f20999d95fa98d825
Anton_Karasin <anton.tablet2002@gmail.com>
2025-01-11 20:39:14 +0300

    AccountInitializer fix

--- src/main/java/ru/be_prog/dao/AccountsInitializer.java
+++ src/main/java/ru/be_prog/dao/AccountsInitializer.java
@@ -2,6 +2,7 @@ package ru.be_prog.dao;
 
 import org.springframework.stereotype.Component;
 import ru.be_prog.model.Account;
+import ru.be_prog.model.Profile;
 import ru.be_prog.service.AccountService;
 
 import java.util.UUID;
@@ -12,16 +13,20 @@ public class AccountsInitializer {
     public AccountsInitializer(AccountService accountService) {
         accountService.deleteAllAccounts();
 
-        Account account1 = new Account(UUID.randomUUID(), "Steve", "Jobs");
+        Profile profile1 = new Profile(UUID.randomUUID(), "02-02-2000", "Spain", "Likes ice cream");
+        Account account1 = new Account(UUID.randomUUID(), "Steve", "Jobs", profile1);
         accountService.saveAccount(account1);
 
-        Account account2 = new Account(UUID.randomUUID(), "Bill", "Gates");
+        Profile profile2 = new Profile(UUID.randomUUID(), "02-02-2001", "Portugal", "Hates ice cream");
+        Account account2 = new Account(UUID.randomUUID(), "Bill", "Gates", profile2);
         accountService.saveAccount(account2);
 
-        Account account3 = new Account(UUID.randomUUID(), "James", "Gosling");
+        Profile profile3 = new Profile(UUID.randomUUID(), "02-02-2002", "Spain", "Thinks that pineapple belongs on pizza");
+        Account account3 = new Account(UUID.randomUUID(), "James", "Gosling", profile3);
         accountService.saveAccount(account3);
 
-        Account account4 = new Account(UUID.randomUUID(), "Erich", "Gamma");
+        Profile profile4 = new Profile(UUID.randomUUID(), "02-02-2003", "Russia", "Never uses vpns");
+        Account account4 = new Account(UUID.randomUUID(), "Erich", "Gamma", profile4);
         accountService.saveAccount(account4);
     }
 }
