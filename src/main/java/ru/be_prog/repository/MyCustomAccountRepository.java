46a1197c663bc4c39eae2c60f2885586f082824c
Anton_Karasin <anton.tablet2002@gmail.com>
2025-01-22 23:48:30 +0300

    remove unused files

--- src/main/java/ru/be_prog/repository/MyCustomAccountRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.be_prog.repository;
-
-import ru.be_prog.model.Account;
-
-import java.util.List;
-
-public interface MyCustomAccountRepository {
-    List<Account> getAccountsByCountry(String country);
-}
